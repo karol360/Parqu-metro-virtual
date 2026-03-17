11df1902dc1921d0da51ab34aaee4f88b1902fc1
Karol Amorim <karoline.kalm360@gmail.com>
2026-03-17 10:06:40 -0300

    transformando o codigo base em POO

--- script.js
+++ script.js
@@ -1,25 +1,58 @@
+class Parquimetro {
+    constructor(valorInserido) {
+        this.valorInserido = valorInserido
+    }
+
+    calcular() {
+        if (this.valorInserido < 1) {
+            return {
+                erro: true,
+                mensagem: 'Valor insuficiente'
+            }
+        }
+
+        if (this.valorInserido < 1.75) {
+            return {
+                tempo: 30,
+                troco: this.valorInserido - 1
+            }
+        }
+
+        if (this.valorInserido < 3) {
+            return {
+                tempo: 60,
+                troco: this.valorInserido - 1.75
+            }
+        }
+
+        return {
+            tempo: 120,
+            troco: this.valorInserido - 3
+        }
+    }
+
+}
+
 function calcular() {
     let res = document.getElementById('caixaRes')
     let valor = document.getElementById('caixaValor')
     let valorUsuario = Number(valor.value)
+
     const formatador = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
 
-    if (valorUsuario < 1) {
-        res.innerHTML = 'Valor Insuficiente'
+    const parquimetro = new Parquimetro(valorUsuario)
+
+    const resultado = parquimetro.calcular()
+
+    if(resultado.erro){
         res.style.color = 'red'
-    } else {
-        if (valorUsuario < 1.75) {
-            res.style.color = '#063B4B'
-            res.innerHTML = `⏱ Tempo: 30 min <br> R$ Troco: ${formatador.format(valorUsuario - 1)}`
-        } else {
-            if (valorUsuario < 3) {
-                res.innerHTML = `⏱ Tempo: 60 min <br> R$ Troco: ${formatador.format(valorUsuario - 1.75)}`
-            } else {
-                res.innerHTML = `⏱ Tempo: 120 min <br> R$ Troco: ${formatador.format(valorUsuario - 3)}`
-            }
-        }
+        res.innerHTML = resultado.mensagem
+        return
     }
 
-    valor.value=''
+    res.style.color ='#063B4B'
+
+    res.innerHTML = `⏱ Tempo: ${resultado.tempo}Min <br> R$ Troco: ${formatador.format(resultado.troco)} `
+    valor.value = ''
     valor.focus()
 }
